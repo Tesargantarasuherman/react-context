9b2e641bb6572c34feee28e98d0dde105eac2ff1
unknown <tgantara@gmail.com>
2021-10-22 00:00:49 +0700

    updat

--- src/HookReducer.js
+++ src/HookReducer.js
@@ -1,49 +1,51 @@
 import React, { useReducer } from 'react'
 const initialState = {
-    jumlah :1,
-    harga_satuan : 10000,
-    harga_total :0
+    jumlah: 1,
+    harga_satuan: 10000,
+    harga_total: 0
 }
 const reducer = (state, action) => {
-    switch (action){
-        case 'tambah' : return {
+    switch (action.type) {
+        case 'tambah': return {
             ...state,
-            jumlah:state.jumlah + 1
+            jumlah: state.jumlah + 1,
+            harga_total: state.harga_satuan + (state.harga_satuan * state.jumlah)
 
         }
-        case 'kurang' : return {
-            jumlah:state.jumlah - 1
+        case 'kurang': return {
+            jumlah: state.jumlah - 1,
+            harga_total: (state.harga_satuan * state.jumlah) - state.harga_satuan
         }
         default:
             return state
     }
 }
 
-    export default function HookReducer() {
-        const [count,dispatch] = useReducer(reducer,initialState)
-        return (
-            <div className="card" >
-                <div className="card-body">
-                    <h5 className="card-title">Acer</h5>
-                    <p className="card-text">Rp, 7.000.000</p>
-                    <div className="row">
-                        <div className="col-md-4">
-                            <button className="btn btn-danger" onClick={()=>dispatch('tambah')}>+</button>
-                        </div>
-                        <div className="col-md-4">
-                        <button className="btn btn-primary">{count.jumlah <0 ? 0 :count.jumlah}</button>
-                        </div>
-                        <div className="col-md-4">
-                            <button className="btn btn-white" onClick={()=>dispatch('kurang')}>-</button>
+export default function HookReducer() {
+    const [count, dispatch] = useReducer(reducer, initialState)
+    return (
+        <div className="card" >
+            <div className="card-body">
+                <h5 className="card-title">Acer</h5>
+                <p className="card-text">Rp, 7.000.000</p>
+                <div className="row">
+                    <div className="col-md-4">
+                        <button className="btn btn-danger" onClick={() => dispatch({ type: 'tambah' })}>+</button>
+                    </div>
+                    <div className="col-md-4">
+                        <button className="btn btn-primary">{count.jumlah < 0 ? 0 : count.jumlah}</button>
+                    </div>
+                    <div className="col-md-4">
+                        <button className="btn btn-white" onClick={() => dispatch({ type: 'kurang' })}>-</button>
 
-                        </div>
-                        <div className="col-md-12">
-                            <button className="btn btn-success">
-                                Total Rp. 10000000
-                            </button>
-                        </div>
+                    </div>
+                    <div className="col-md-12">
+                        <button className="btn btn-success">
+                            Total Rp. {count.harga_total}
+                        </button>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+}
