d1025b9923e5252d07af00e1f018ef2675717ef0
unknown <tgantara@gmail.com>
2021-10-21 23:21:42 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import ContextPage from './ContextPage';
+import { CartContext } from './CartContex';
+import Navbar from './components/Navbar';
+import { useState } from 'react';
+import ProductComponent from './components/ProductComponent';
+import HookReducer from './HookReducer';
 
 function App() {
+
+  const [value,setValue] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <ContextPage /> */}
+      <CartContext.Provider value={{value,setValue}}>
+          <Navbar />
+          <ProductComponent />
+          <HookReducer />
+      </CartContext.Provider>
+    </>
   );
 }
 
